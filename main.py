c0ff258929792d4591417d7a9ea624c22f25e709
KeyZenD <37895600+KeyZenD@users.noreply.github.com>
2020-07-08 03:01:14 +0800

    Билли джин
    Насрал в кувшин

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-from PIL import Image
-from PIL import ImageFont
-from PIL import ImageDraw
+from PIL import Image, ImageFont, ImageDraw
 import requests
 
 api = requests.get("https://www.pgpf.org/pgpf-debt-stats/json/next-debt-slice")
 debt = api.json()["gross_debt"]
-gosdolg = f"{debt}"
 
 img = Image.open("template.jpg")
 
 draw = ImageDraw.Draw(img)
 font = ImageFont.truetype("arial_bold_italic.ttf", 60)
-draw.text((240, 248), gosdolg, (255, 82, 82), font=font)
+draw.text((240, 248), str(debt), (255, 82, 82), font=font)
 
 img.save('gosdolg.jpg')
